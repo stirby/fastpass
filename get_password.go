ad865c915347d4932c6a433d6b232d4856753b2c
Ammar Bandukwala <ammar@ammar.io>
2017-04-03 06:43:58 -0500

    stretch key

--- get_password.go
+++ get_password.go
@@ -1,16 +1,23 @@
 package fastpass
 
-import "github.com/howeyc/gopass"
-import "crypto/sha256"
-import "fmt"
+import (
+	"crypto/sha256"
+	"fmt"
+
+	"github.com/howeyc/gopass"
+	"golang.org/x/crypto/pbkdf2"
+)
+
+var salt = []byte("seems like a decent salt")
 
 //GetPassword retrieves a password from the command line.
 //It returns the sha256 representation of the password.
-func GetPassword() [32]byte {
+func GetPassword() (key [32]byte) {
 	fmt.Printf("Enter password: ")
 	rawpwd, err := gopass.GetPasswdMasked()
 	if err != nil {
 		panic(err)
 	}
-	return sha256.Sum256(rawpwd)
+	copy(key[:], pbkdf2.Key(rawpwd, salt, 4096, 32, sha256.New))
+	return
 }
