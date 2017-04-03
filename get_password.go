c7873dc3f32d310fd32ec80ffaedf263a0710807
Ammar Bandukwala <ammar@ammar.io>
2017-04-03 06:49:15 -0500

    use more iterations

--- get_password.go
+++ get_password.go
@@ -10,14 +10,16 @@ import (
 
 var salt = []byte("seems like a decent salt")
 
+const iters = 1024 * 64
+
 //GetPassword retrieves a password from the command line.
-//It returns the sha256 representation of the password.
+//It returns the the stretched representation of the password using pbkdf2.
 func GetPassword() (key [32]byte) {
 	fmt.Printf("Enter password: ")
 	rawpwd, err := gopass.GetPasswdMasked()
 	if err != nil {
 		panic(err)
 	}
-	copy(key[:], pbkdf2.Key(rawpwd, salt, 4096, 32, sha256.New))
+	copy(key[:], pbkdf2.Key(rawpwd, salt, iters, 32, sha256.New))
 	return
 }
